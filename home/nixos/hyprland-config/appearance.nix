23264ef648c0f2761edf71e8c0b28c0074fea2af
lewisflude <lewis@lewisflude.com>
2025-06-22 11:18:37 +0100

    Move platform-specific configs: waybar, launcher, and hyprland to nixos/

--- home/nixos/hyprland-config/appearance.nix
+++ home/nixos/hyprland-config/appearance.nix
@@ -10,6 +10,8 @@
       extend_border_grab_area = 15;
       hover_icon_on_border = true;
       layout = "dwindle";
+      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      "col.inactive_border" = "rgba(595959aa)";
     };
 
     decoration = {
@@ -23,6 +25,24 @@
         passes = 1;
         ignore_opacity = true;
       };
+      drop_shadow = true;
+      shadow_range = 4;
+      shadow_render_power = 3;
+      "col.shadow" = "rgba(1a1a1aee)";
+    };
+
+    # Animations
+    animations = {
+      enabled = true;
+      bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+      animation = [
+        "windows, 1, 7, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+      ];
     };
 
     ecosystem = {
