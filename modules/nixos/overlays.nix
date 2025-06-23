7f702df112b84169c3d348f4bb9e1167faa3f22b
lewis <lewis@lewisflude.com>
2025-06-23 06:30:50 -0400

    Update nix config

--- modules/nixos/overlays.nix
+++ modules/nixos/overlays.nix
@@ -2,8 +2,23 @@
   inputs,
   lib,
   ...
-}: {
-  nixpkgs.overlays = lib.optionals (inputs ? nvidia-patch) [
-    inputs.nvidia-patch.overlays.default
-  ];
+}:
+{
+  nixpkgs.overlays =
+    [
+      # Yazi overlay
+      inputs.yazi.overlays.default
+
+      # Waybar overlay
+      (final: _: { waybar_git = inputs.waybar.packages.${final.system}.waybar; })
+
+      # Ghostty overlay
+      (final: prev: {
+        ghostty = inputs.ghostty.packages.${final.system}.default;
+      })
+    ]
+    ++ lib.optionals (inputs ? nvidia-patch) [
+      # Nvidia patch overlay (conditionally added)
+      inputs.nvidia-patch.overlays.default
+    ];
 }
