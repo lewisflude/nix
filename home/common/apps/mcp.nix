e25d236032b70da38abc7469323d6acfd09d7aae
lewisflude <lewis@lewisflude.com>
2025-06-26 18:00:05 +0100

    Auto-backup: Thu Jun 26 18:00:05 BST 2025

--- home/common/apps/mcp.nix
+++ home/common/apps/mcp.nix
@@ -9,13 +9,52 @@
   ];
   services.mcp.enable = true;
   services.mcp.servers = {
-    kagi = {
-      command = "uvx";
-      args = [ "kagimcp" ];
-      env = {
-        KAGI_API_KEY = config.sops.secrets.KAGI_API_KEY.path;
-      };
-      port = 11431;
+    # kagi = {
+    #   command = "${pkgs.uv}/bin/uvx";
+    #   args = [ "kagimcp" ];
+    #   env = {
+    #     KAGI_API_KEY = config.sops.secrets.KAGI_API_KEY.path;
+    #   };
+    #   port = 11431;
+    # };
+    fetch = {
+      command = "${pkgs.uv}/bin/uvx";
+      args = [ "mcp-server-fetch" ];
+      port = 11432;
     };
+    git = {
+      command = "${pkgs.docker}/bin/docker";
+      args = [
+        "run"
+        "-i"
+        "--rm"
+        "--mount"
+        "type=bind,source=/Users/lewisflude/Code,dst=/Users/lewisflude/Code"
+        "mcp/git"
+      ];
+      port = 11434;
+    };
+    nx = {
+      command = "${pkgs.nodejs_24}/bin/npx";
+      args = [
+        "nx-mcp@latest"
+        "/Users/lewisflude/Code/dex-web"
+      ];
+      port = 11435;
+    };
+    sequentialThinking = {
+      command = "${pkgs.docker}/bin/docker";
+      args = [
+        "run"
+        "--rm"
+        "-i"
+        "mcp/sequentialthinking"
+      ];
+      env = {
+        PATH = "${pkgs.nodejs_22}/bin" + ":" + (builtins.getEnv "PATH");
+      };
+      port = 11435;
+    };
+
   };
 }
