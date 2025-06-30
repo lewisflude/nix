089c80b8545de6d3d9a7fcbf41ede67d6d66bbb3
lewis <lewis@lewisflude.com>
2025-06-30 14:11:47 +0100

    fix: update hyprland startup commands to use uwsm for application management

--- home/nixos/hyprland-config/startup.nix
+++ home/nixos/hyprland-config/startup.nix
@@ -1,17 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       # Start other services
-      "hyprpanel"
+      "uwsm app hyprpanel"
       "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-      "systemctl --user enable --now waybar.service"
+      "uwsm app waybar"
       "systemctl --user enable --now ratbagd.service"
       "systemctl --user enable --now sunshine"
       "pw-link 'Main-Output-Proxy:monitor_FL' 'alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX0'"
       "pw-link 'Main-Output-Proxy:monitor_FR' 'alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX1'"
-      "nwg-dock-hyprland -d"
-      "nwg-drawer -r"
-      "clipse -listen"
+      "uwsm app nwg-dock-hyprland -- -d"
+      "uwsm app nwg-drawer -- -r"
+      "uwsm app clipse -- -listen"
     ];
   };
 }
