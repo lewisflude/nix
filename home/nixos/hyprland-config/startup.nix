6ccd0a55943f19c48a8f96c07598cc750e4b9648
lewis <lewis@lewisflude.com>
2025-06-30 15:39:02 +0100

    feat: update hyprland settings, variables, hyprlock

--- home/nixos/hyprland-config/startup.nix
+++ home/nixos/hyprland-config/startup.nix
@@ -2,17 +2,20 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      # Start other services
-      "uwsm app hyprpanel"
-      "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-      "uwsm app waybar"
-      "systemctl --user enable --now ratbagd.service"
-      "systemctl --user enable --now sunshine"
+      # Authentication agent
+      "uwsm app --${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+
+      # UI components
+      "uwsm app -- waybar"
+      "uwsm app -- nwg-dock-hyprland -- -d"
+      "uwsm app -- nwg-drawer -- -r"
+
+      # Utilities
+      "uwsm app -- clipse -- -listen"
+
+      # Audio routing for Apogee Symphony Desktop
       "pw-link 'Main-Output-Proxy:monitor_FL' 'alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX0'"
       "pw-link 'Main-Output-Proxy:monitor_FR' 'alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX1'"
-      "uwsm app nwg-dock-hyprland -- -d"
-      "uwsm app nwg-drawer -- -r"
-      "uwsm app clipse -- -listen"
     ];
   };
 }
