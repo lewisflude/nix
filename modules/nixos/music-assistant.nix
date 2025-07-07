4851475f454abeebafceb04fd7dc0a0daa0999c6
lewis <lewis@lewisflude.com>
2025-07-07 12:55:15 +0100

    feat: update home assistant with intents + samba config

--- modules/nixos/music-assistant.nix
+++ modules/nixos/music-assistant.nix
@@ -7,30 +7,27 @@
 let
   musicAssistantPort = 8095;
   musicAssistantStreamPort = 8097;
-  sonosAppCtrlPort = 1400;
   dlnaPort = 8200;
   airplayPort = 7000;
+  dlnaDiscoveryPort = 1900;
+  upnpEventsPort = 2869;
+  multicastDnsPort = 5353;
 in
 {
-
   virtualisation.oci-containers = {
     containers = {
       music-assistant-server = {
         image = "ghcr.io/music-assistant/server:latest";
-
         environment = {
           LOG_LEVEL = "info";
         };
-
         volumes = [
           "/var/lib/music-assistant:/data"
           "/mnt/storage/media/music:/mnt/storage/media/music"
         ];
-
         extraOptions = [
           "--network=host"
           "--privileged"
-
         ];
       };
     };
@@ -43,8 +40,16 @@ in
   networking.firewall.allowedTCPPorts = [
     musicAssistantPort
     musicAssistantStreamPort
-    sonosAppCtrlPort
     dlnaPort
+    upnpEventsPort
+    dlnaDiscoveryPort
+    multicastDnsPort
+    airplayPort
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    dlnaDiscoveryPort
+    multicastDnsPort
     airplayPort
   ];
 }
