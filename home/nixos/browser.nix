c22af3ce899c3d444f1027f74f71920a70f2459d
lewis <lewis@lewisflude.com>
2025-07-23 13:56:15 +0100

    Update browser config

--- home/nixos/browser.nix
+++ home/nixos/browser.nix
@@ -16,10 +16,12 @@ in
           ublock-origin
           kagi-search
           onepassword-password-manager
+          firefox-color
         ];
       };
 
       search = {
+        force = true;
         default = "Kagi";
         order = [ "Kagi" ];
         engines.Kagi = {
@@ -36,34 +38,6 @@ in
           ];
           definedAliases = [ "@k" ];
         };
-      };
-
-      settings = {
-        # Hardware acceleration and performance
-        "media.hardware-video-decoding.enabled" = true;
-        "media.ffmpeg.vaapi.enabled" = true;
-        "media.rdd-ffmpeg.enabled" = true;
-        "layers.acceleration.force-enabled" = true;
-        "gfx.webrender.all" = true;
-        "webgl.force-enabled" = true;
-        "layers.offmainthreadcomposition.enabled" = true;
-        "gfx.canvas.azure.accelerated" = true;
-        "layers.async-video.enabled" = true;
-        "gfx.x11-egl.force-enabled" = true;
-
-        # Memory management
-        "browser.tabs.unloadOnLowMemory" = true;
-
-        # Privacy and telemetry
-        "toolkit.telemetry.enabled" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.strictMode.enabled" = true;
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.resistFingerprinting" = true;
-        "dom.security.https_only_mode" = true;
-        "dom.security.https_only_mode_ever_enabled" = true;
-
       };
     };
   };
