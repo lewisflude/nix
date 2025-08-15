02ba76a7a60c4b056bd5fab56e02e9b74bab6a68
lewis <lewis@lewisflude.com>
2025-08-15 11:01:39 +0100

    feat: add manual cursor install

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "Lewiss NixOS and Nix-Darwin configuration";
 
   nixConfig = {
     experimental-features = [
@@ -10,37 +10,59 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    darwin.url = "github:nix-darwin/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # System frameworks
+    darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # macOS utilities
     mac-app-util.url = "github:hraban/mac-app-util";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    # Themes and styling
     catppuccin = {
       url = "github:catppuccin/nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Desktop environment and UI
     astal = {
       url = "github:aylur/astal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    waybar.url = "github:Alexays/Waybar/master";
+    niri-unstable.url = "github:sodiboo/niri-flake";
+    swww.url = "github:LGFae/swww";
+
+    # Security and secrets
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    waybar.url = "github:Alexays/Waybar/master";
+
+    # Audio and multimedia
     musnix = {
       url = "github:musnix/musnix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs-mozilla = {
-      url = "github:mozilla/nixpkgs-mozilla";
-    };
+
+    # Development and packages
+    nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     yazi.url = "github:sxyazi/yazi";
+    ghostty.url = "github:ghostty-org/ghostty";
+
+    # System and hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     solaar = {
       url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz";
@@ -50,12 +72,21 @@
       url = "github:icewind1991/nvidia-patch-nixos";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Tools and utilities
     nh = {
       url = "github:nix-community/nh";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    cursor.url = "github:omarcresp/cursor-flake/main";
     mcp-hub.url = "github:ravitemer/mcp-hub";
+
+    # Development and CI
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Homebrew taps (non-flake)
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
       flake = false;
@@ -72,209 +103,52 @@
       url = "github:j178/homebrew-tap";
       flake = false;
     };
-    ghostty = {
-      url = "github:ghostty-org/ghostty";
-    };
-    niri-unstable = {
-      url = "github:sodiboo/niri-flake";
-    };
-    swww = {
-      url = "github:LGFae/swww";
-    };
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
-    inputs@{
-      self,
-      darwin,
-      nixpkgs,
-      home-manager,
-      mac-app-util,
-      nix-homebrew,
-      homebrew-core,
-      homebrew-cask,
-      homebrew-nx,
-      homebrew-j178,
-      catppuccin,
-      sops-nix,
-      musnix,
-      nur,
-      solaar,
-      niri-unstable,
-      nh,
-      pre-commit-hooks,
-      ...
-    }:
+    inputs@{ self, ... }:
     let
-      hosts = {
-        "Lewiss-MacBook-Pro" = import ./hosts/Lewiss-MacBook-Pro;
-        jupiter = import ./hosts/jupiter;
+      # Import host configurations and utilities
+      hostsConfig = import ./lib/hosts.nix { inherit (inputs.nixpkgs) lib; };
+      hosts = hostsConfig.hosts;
+      systems = hostsConfig.getSystems hosts;
+
+      # Import system builders
+      systemBuilders = import ./lib/system-builders.nix { inherit inputs; };
+
+      # Import output builders with necessary context
+      outputBuilders = import ./lib/output-builders.nix {
+        inputs = inputs // {
+          inherit self;
+        };
+        inherit hosts;
       };
 
-      systems = builtins.attrValues (builtins.mapAttrs (_name: host: host.system) hosts);
-
+      # Darwin system builder with homebrew inputs
       mkDarwinSystem =
         hostName: hostConfig:
-
-        darwin.lib.darwinSystem {
-          system = hostConfig.system;
-          specialArgs = inputs // hostConfig;
-          modules = [
-            ./hosts/${hostName}/configuration.nix
-            ./modules/common
-            ./modules/darwin
-            home-manager.darwinModules.home-manager
-            mac-app-util.darwinModules.default
-            nix-homebrew.darwinModules.nix-homebrew
-            sops-nix.darwinModules.sops
-            { _module.args = { inherit inputs; }; }
-            {
-              nix-homebrew = {
-                enable = true;
-                enableRosetta = true;
-                user = hostConfig.username;
-                autoMigrate = true;
-                taps = {
-                  "homebrew/homebrew-cask" = homebrew-cask;
-                  "homebrew/homebrew-core" = homebrew-core;
-                  "nrwl/homebrew-nx" = homebrew-nx;
-                  "j178/homebrew-tap" = homebrew-j178;
-                };
-                mutableTaps = false;
-              };
-            }
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.verbose = true;
-              home-manager.sharedModules = [
-                sops-nix.homeManagerModules.sops
-                mac-app-util.homeManagerModules.default
-                catppuccin.homeModules.catppuccin
-              ];
-              home-manager.extraSpecialArgs = inputs // hostConfig;
-              home-manager.users.${hostConfig.username} = import ./home;
-            }
-          ];
+        systemBuilders.mkDarwinSystem hostName hostConfig {
+          inherit (inputs)
+            homebrew-core
+            homebrew-cask
+            homebrew-nx
+            homebrew-j178
+            ;
         };
 
+      # NixOS system builder with self reference
       mkNixosSystem =
-        hostName: hostConfig:
-
-        nixpkgs.lib.nixosSystem {
-          system = hostConfig.system;
-          specialArgs =
-            inputs
-            // hostConfig
-            // {
-              keysDirectory = "${self}/keys";
-            };
-          modules = [
-            # Apply overlays first
-            ./hosts/${hostName}/configuration.nix
-            ./modules/common
-            ./modules/nixos
-            sops-nix.nixosModules.sops
-            catppuccin.nixosModules.catppuccin
-            niri-unstable.nixosModules.niri
-            musnix.nixosModules.musnix
-            nur.modules.nixos.default
-            solaar.nixosModules.default
-            { _module.args = { inherit inputs; }; }
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.verbose = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.extraSpecialArgs = inputs // hostConfig;
-
-              home-manager.sharedModules = [
-                catppuccin.homeModules.catppuccin
-                inputs.sops-nix.homeManagerModules.sops
-              ];
-              home-manager.users.${hostConfig.username} = import ./home;
-            }
-          ];
-        };
+        hostName: hostConfig: systemBuilders.mkNixosSystem hostName hostConfig { inherit self; };
     in
     {
-      formatter = nixpkgs.lib.genAttrs systems
-        (system: nixpkgs.legacyPackages.${system}.alejandra);
+      # Development and formatting outputs
+      formatter = outputBuilders.mkFormatters;
+      checks = outputBuilders.mkChecks;
+      devShells = outputBuilders.mkDevShells;
 
-      checks = nixpkgs.lib.genAttrs systems (system: {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            alejandra.enable = true;
-            deadnix.enable = true;
-            statix.enable = true;
-          };
-        };
-      });
-
-      devShells = builtins.listToAttrs (
-        builtins.map (hostConfig: {
-          name = hostConfig.system;
-          value =
-            let
-              pkgs = nixpkgs.legacyPackages.${hostConfig.system};
-              shellsConfig = import ./shells {
-                inherit pkgs;
-                lib = pkgs.lib;
-                system = hostConfig.system;
-              };
-            in
-            shellsConfig.devShells
-            // {
-              default = pkgs.mkShell {
-                inherit (self.checks.${hostConfig.system}.pre-commit-check) shellHook;
-                buildInputs =
-                  self.checks.${hostConfig.system}.pre-commit-check.enabledPackages
-                  ++ (with pkgs; [
-                    jq
-                    yq
-                    git
-                    gh
-                    direnv
-                    nix-direnv
-                  ]);
-              };
-            };
-        }) (builtins.attrValues hosts)
-      );
-
-      darwinConfigurations = builtins.mapAttrs (name: hostConfig: mkDarwinSystem name hostConfig) (
-        nixpkgs.lib.filterAttrs (
-          _name: host: host.system == "aarch64-darwin" || host.system == "x86_64-darwin"
-        ) hosts
-      );
-
-      nixosConfigurations = builtins.mapAttrs (name: hostConfig: mkNixosSystem name hostConfig) (
-        nixpkgs.lib.filterAttrs (
-          _name: host: host.system == "x86_64-linux" || host.system == "aarch64-linux"
-        ) hosts
-      );
-
-      homeConfigurations = builtins.mapAttrs (
-        _name: hostConfig:
-        let
-          pkgs = import nixpkgs {
-            system = hostConfig.system;
-          };
-        in
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = inputs // hostConfig;
-
-          modules = [
-            ./home
-            catppuccin.homeModules.catppuccin
-          ];
-        }
-      ) hosts;
+      # System configurations
+      darwinConfigurations = builtins.mapAttrs mkDarwinSystem (hostsConfig.getDarwinHosts hosts);
+      nixosConfigurations = builtins.mapAttrs mkNixosSystem (hostsConfig.getNixosHosts hosts);
+      homeConfigurations = outputBuilders.mkHomeConfigurations;
     };
 }
