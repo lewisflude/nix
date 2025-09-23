9378821825feb98529b39a88d80dc36c489ecd4e
lewis <lewis@lewisflude.com>
2025-09-23 14:54:00 +0100

    feat: update audio.nix

--- home/nixos/system/audio.nix
+++ home/nixos/system/audio.nix
@@ -10,15 +10,17 @@
     Service = {
       # This service is a one-shot script
       Type = "oneshot";
-      ExecStart = ''
-        ${pkgs.pipewire}/bin/pw-link \
-          "Main-Output-Proxy:monitor_FL" \
-          "alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX0"
+      ExecStart = let
+        script = pkgs.writeShellScript "setup-audio-routing" ''
+          ${pkgs.pipewire}/bin/pw-link \
+            "Main-Output-Proxy:monitor_FL" \
+            "alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX0"
 
-        ${pkgs.pipewire}/bin/pw-link \
-          "Main-Output-Proxy:monitor_FR" \
-          "alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX1"
-      '';
+          ${pkgs.pipewire}/bin/pw-link \
+            "Main-Output-Proxy:monitor_FR" \
+            "alsa_output.usb-Apogee_Electronics_Corp_Symphony_Desktop-00.pro-output-0:playback_AUX1"
+        '';
+      in "${script}";
     };
 
     Install = {
