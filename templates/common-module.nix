dacaa5999bc698c9f883754aa57dd5e1e2b5ccde
lewisflude <lewis@lewisflude.com>
2025-10-29 10:17:52 +0000

    feat(templates): add module templates    2 - Add common module template - Add Darwin-specific module template - Add NixOS-specific module template - Add home-manager module templates - Add feature module template - Add service module template - Add overlay template - Add test module template   11 These templates provide scaffolding for quickly creating new modules following established patterns.

---
+++ templates/common-module.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  lib,
+  system,
+  username,
+  ...
+}: let
+  platformLib = (import ../../lib/functions.nix {inherit lib;}).withSystem system;
+in {
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+    git
+  ];
+  services.example = {
+    enable = true;
+    port = 8080;
+    user = username;
+  };
+  programs.example = {
+    enable = true;
+    package = platformLib.platformPackage pkgs.example-linux pkgs.example-darwin;
+  };
+}
---
+++ templates/overlay-template.nix
@@ -0,0 +1,76 @@
+# Overlay template
+# For overriding or adding packages to nixpkgs
+# Place in overlays/ directory and import in overlays/default.nix
+final: prev: {
+  # Override existing package
+  example-package = prev.example-package.overrideAttrs (oldAttrs: {
+    version = "1.2.3";
+    src = final.fetchFromGitHub {
+      owner = "owner";
+      repo = "repo";
+      rev = "v1.2.3";
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+
+    # Add additional build inputs
+    buildInputs = oldAttrs.buildInputs or [] ++ [final.extra-dep];
+
+    # Modify build flags
+    configureFlags =
+      oldAttrs.configureFlags or []
+      ++ [
+        "--enable-feature"
+      ];
+  });
+
+  # Add new custom package
+  my-custom-package = final.stdenv.mkDerivation {
+    pname = "my-custom-package";
+    version = "1.0.0";
+
+    src = final.fetchFromGitHub {
+      owner = "username";
+      repo = "my-custom-package";
+      rev = "v1.0.0";
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+
+    nativeBuildInputs = with final; [
+      pkg-config
+      makeWrapper
+    ];
+
+    buildInputs = with final; [
+      # runtime dependencies
+    ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin
+      install -Dm755 my-custom-package $out/bin/my-custom-package
+
+      runHook postInstall
+    '';
+
+    meta = with final.lib; {
+      description = "Brief description of the package";
+      homepage = "https://github.com/username/my-custom-package";
+      license = licenses.mit;
+      maintainers = [];
+      platforms = platforms.unix;
+    };
+  };
+
+  # Wrapper around existing package with custom settings
+  my-wrapped-package = final.symlinkJoin {
+    name = "my-wrapped-package";
+    paths = [final.original-package];
+    buildInputs = [final.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/original-package \
+        --set CUSTOM_VAR "value" \
+        --prefix PATH : ${final.lib.makeBinPath [final.extra-tool]}
+    '';
+  };
+}
