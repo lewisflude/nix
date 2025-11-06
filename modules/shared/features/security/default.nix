1ec309f6cf357ebfb446287809d4cc3617daa477
lewisflude <lewis@lewisflude.com>
2025-11-06 12:56:28 +0000

    [1mSTDIN[0m [38;2;127;132;156m   1[0m [38;2;205;214;244mfix: remove unexpected perSystemPkgs argument from mkDarwinSystem call[0m [38;2;127;132;156m   2[0m [38;2;127;132;156m   3[0m [38;2;205;214;244mThe mkDarwinSystem function in lib/system-builders.nix only expects[0m [38;2;127;132;156m   4[0m [38;2;205;214;244mhomebrew-j178 in its parameter set, but flake-parts/core.nix was[0m [38;2;127;132;156m   5[0m [38;2;205;214;244mpassing an additional perSystemPkgs argument causing a build error.[0m [38;2;127;132;156m   6[0m [38;2;205;214;244mThis removes the unexpected argument to fix the Darwin system build.[0m [38;2;127;132;156m   7[0m [38;2;127;132;156m   8[0m [38;2;205;214;244mAlso includes:[0m [38;2;127;132;156m   9[0m [38;2;205;214;244m- Comment out flakehub package reference in core-tooling.nix[0m [38;2;127;132;156m  10[0m [38;2;205;214;244m- Format home-manager config in system-builders.nix for consistency[0m [38;2;127;132;156m  11[0m [38;2;205;214;244m- Refactor security module to use mkMerge pattern[0m [38;2;127;132;156m  12[0m [38;2;205;214;244m- Remove unused lambda patterns (inputs, config)[0m

--- modules/shared/features/security/default.nix
+++ modules/shared/features/security/default.nix
@@ -6,84 +6,93 @@
   ...
 }:
 let
-  inherit (lib) mkIf mkDefault;
+  inherit (lib)
+    mkIf
+    mkDefault
+    mkMerge
+    optionalAttrs
+    ;
   inherit (lib.lists) optional optionals;
   cfg = config.host.features.security;
   platformLib = (import ../../../../lib/functions.nix { inherit lib; }).withSystem hostSystem;
   inherit (platformLib) isLinux;
 in
 {
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      assertions = [
+        {
+          assertion = cfg.firewall -> isLinux;
+          message = "Advanced firewall configuration is only available on NixOS";
+        }
+      ];
+    }
 
-    security = mkIf isLinux {
-
-      doas = {
-        enable = true;
-        extraRules = [
-          {
-            users = [ config.host.username ];
-            keepEnv = true;
-            persist = true;
-          }
-        ];
-      };
-
-      pam = {
-        services = {
-          login.u2fAuth = cfg.yubikey;
-          sudo.u2fAuth = cfg.yubikey;
-          greetd.u2fAuth = cfg.yubikey;
-        };
-        u2f = mkIf cfg.yubikey {
+    (optionalAttrs isLinux {
+      security = {
+        doas = {
           enable = true;
-          control = "sufficient";
-          settings = {
-            debug = false;
-            interactive = true;
-            cue = true;
-            origin = "pam://yubi";
-            authfile = "/etc/u2f_mappings";
-            max_devices = 5;
+          extraRules = [
+            {
+              users = [ config.host.username ];
+              keepEnv = true;
+              persist = true;
+            }
+          ];
+        };
+
+        pam = {
+          services = {
+            login.u2fAuth = cfg.yubikey;
+            sudo.u2fAuth = cfg.yubikey;
+            greetd.u2fAuth = cfg.yubikey;
+          };
+          u2f = mkIf cfg.yubikey {
+            enable = true;
+            control = "sufficient";
+            settings = {
+              debug = false;
+              interactive = true;
+              cue = true;
+              origin = "pam://yubi";
+              authfile = "/etc/u2f_mappings";
+              max_devices = 5;
+            };
           };
         };
+
+        polkit.enable = true;
       };
 
-      polkit.enable = true;
-    };
+      services.gnome.gnome-keyring.enable = true;
 
-    services.gnome.gnome-keyring.enable = mkIf isLinux true;
+      environment.systemPackages =
+        with pkgs;
+        optionals cfg.yubikey [
+          yubikey-manager
+          yubikey-personalization
+          yubioath-flutter
+        ]
+        ++ optionals cfg.gpg [
+          gnupg
+          pinentry-qt
+        ];
 
-    programs.gnupg.agent = mkIf (isLinux && cfg.gpg) {
-      enable = true;
-      enableSSHSupport = true;
-      pinentryPackage = mkDefault pkgs.pinentry-qt;
-    };
+      users.users.${config.host.username}.extraGroups = optional cfg.yubikey "uucp";
+    })
 
-    networking.firewall = mkIf (isLinux && cfg.firewall) {
-      enable = true;
+    (optionalAttrs (isLinux && cfg.gpg) {
+      programs.gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+        pinentryPackage = mkDefault pkgs.pinentry-qt;
+      };
+    })
 
-    };
-
-    environment.systemPackages = mkIf isLinux (
-      with pkgs;
-      optionals cfg.yubikey [
-        yubikey-manager
-        yubikey-personalization
-        yubioath-flutter
-      ]
-      ++ optionals cfg.gpg [
-        gnupg
-        pinentry-qt
-      ]
-    );
-
-    users.users.${config.host.username}.extraGroups = mkIf isLinux (optional cfg.yubikey "uucp");
-
-    assertions = [
-      {
-        assertion = cfg.firewall -> isLinux;
-        message = "Advanced firewall configuration is only available on NixOS";
-      }
-    ];
-  };
+    (optionalAttrs (isLinux && cfg.firewall) {
+      networking.firewall = {
+        enable = true;
+      };
+    })
+  ]);
 }
