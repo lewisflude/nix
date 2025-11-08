3e8613447f10148b25692bb11502b23747986559
lewis <lewis@lewisflude.com>
2025-11-08 21:05:35 +0000

    fix(theme): remove # from hex values in palette

--- modules/shared/features/theming/palette.nix
+++ modules/shared/features/theming/palette.nix
@@ -15,11 +15,11 @@ let
         h
         hex
         ;
-      # Parse hex to RGB (format: #RRGGBB)
+      # Parse hex to RGB (format: RRGGBB)
       rgb = {
-        r = lib.toInt "0x${builtins.substring 1 2 hex}";
-        g = lib.toInt "0x${builtins.substring 3 2 hex}";
-        b = lib.toInt "0x${builtins.substring 5 2 hex}";
+        r = lib.toInt "0x${builtins.substring 0 2 hex}";
+        g = lib.toInt "0x${builtins.substring 2 2 hex}";
+        b = lib.toInt "0x${builtins.substring 4 2 hex}";
       };
     };
 in
@@ -32,13 +32,13 @@ in
         l = 1.0;
         c = 0.0;
         h = 0.0;
-        hex = "#ffffff";
+        hex = "ffffff";
       };
       base-L095 = mkColor {
         l = 0.95;
         c = 0.01;
         h = 240.0;
-        hex = "#f2f3f7";
+        hex = "f2f3f7";
       };
 
       # Surface colors
@@ -46,13 +46,13 @@ in
         l = 0.91;
         c = 0.01;
         h = 240.0;
-        hex = "#e7e8ed";
+        hex = "e7e8ed";
       };
       surface-Lc10 = mkColor {
         l = 0.87;
         c = 0.02;
         h = 240.0;
-        hex = "#dcdee6";
+        hex = "dcdee6";
       };
 
       # Divider colors
@@ -60,13 +60,13 @@ in
         l = 0.83;
         c = 0.02;
         h = 240.0;
-        hex = "#d1d3dd";
+        hex = "d1d3dd";
       };
       divider-Lc30 = mkColor {
         l = 0.75;
         c = 0.03;
         h = 240.0;
-        hex = "#b9bcc9";
+        hex = "b9bcc9";
       };
 
       # Text colors
@@ -74,19 +74,19 @@ in
         l = 0.60;
         c = 0.04;
         h = 240.0;
-        hex = "#8b8fa1";
+        hex = "8b8fa1";
       };
       text-Lc60 = mkColor {
         l = 0.45;
         c = 0.05;
         h = 240.0;
-        hex = "#5f6378";
+        hex = "5f6378";
       };
       text-Lc75 = mkColor {
         l = 0.30;
         c = 0.05;
         h = 240.0;
-        hex = "#373b4e";
+        hex = "373b4e";
       };
     };
 
@@ -96,13 +96,13 @@ in
         l = 0.0;
         c = 0.0;
         h = 0.0;
-        hex = "#000000";
+        hex = "000000";
       };
       base-L015 = mkColor {
         l = 0.15;
         c = 0.01;
         h = 240.0;
-        hex = "#1e1f26";
+        hex = "1e1f26";
       };
 
       # Surface colors
@@ -110,13 +110,13 @@ in
         l = 0.19;
         c = 0.01;
         h = 240.0;
-        hex = "#25262f";
+        hex = "25262f";
       };
       surface-Lc10 = mkColor {
         l = 0.23;
         c = 0.02;
         h = 240.0;
-        hex = "#2d2e39";
+        hex = "2d2e39";
       };
 
       # Divider colors
@@ -124,13 +124,13 @@ in
         l = 0.27;
         c = 0.02;
         h = 240.0;
-        hex = "#353642";
+        hex = "353642";
       };
       divider-Lc30 = mkColor {
         l = 0.35;
         c = 0.03;
         h = 240.0;
-        hex = "#454759";
+        hex = "454759";
       };
 
       # Text colors
@@ -138,19 +138,19 @@ in
         l = 0.50;
         c = 0.04;
         h = 240.0;
-        hex = "#6b6f82";
+        hex = "6b6f82";
       };
       text-Lc60 = mkColor {
         l = 0.65;
         c = 0.05;
         h = 240.0;
-        hex = "#9498ab";
+        hex = "9498ab";
       };
       text-Lc75 = mkColor {
         l = 0.80;
         c = 0.05;
         h = 240.0;
-        hex = "#c0c3d1";
+        hex = "c0c3d1";
       };
     };
   };
@@ -163,19 +163,19 @@ in
         l = 0.71;
         c = 0.20;
         h = 130.0;
-        hex = "#4db368";
+        hex = "4db368";
       };
       Lc60-h130 = mkColor {
         l = 0.56;
         c = 0.18;
         h = 130.0;
-        hex = "#2d8f47";
+        hex = "2d8f47";
       };
       Lc45-h130 = mkColor {
         l = 0.41;
         c = 0.15;
         h = 130.0;
-        hex = "#1b6a2f";
+        hex = "1b6a2f";
       };
 
       # Danger/Error (Red) - h040
@@ -183,19 +183,19 @@ in
         l = 0.64;
         c = 0.23;
         h = 40.0;
-        hex = "#d9574a";
+        hex = "d9574a";
       };
       Lc60-h040 = mkColor {
         l = 0.49;
         c = 0.21;
         h = 40.0;
-        hex = "#a93a2e";
+        hex = "a93a2e";
       };
       Lc45-h040 = mkColor {
         l = 0.34;
         c = 0.18;
         h = 40.0;
-        hex = "#7a2419";
+        hex = "7a2419";
       };
 
       # Warning/Attention (Yellow-Orange) - h090
@@ -203,19 +203,19 @@ in
         l = 0.79;
         c = 0.15;
         h = 90.0;
-        hex = "#c9a93a";
+        hex = "c9a93a";
       };
       Lc60-h090 = mkColor {
         l = 0.64;
         c = 0.13;
         h = 90.0;
-        hex = "#9d8221";
+        hex = "9d8221";
       };
       Lc45-h090 = mkColor {
         l = 0.49;
         c = 0.11;
         h = 90.0;
-        hex = "#715e0e";
+        hex = "715e0e";
       };
 
       # Info (Cyan) - h190
@@ -223,19 +223,19 @@ in
         l = 0.73;
         c = 0.12;
         h = 190.0;
-        hex = "#5aabb9";
+        hex = "5aabb9";
       };
       Lc60-h190 = mkColor {
         l = 0.58;
         c = 0.11;
         h = 190.0;
-        hex = "#3d8592";
+        hex = "3d8592";
       };
       Lc45-h190 = mkColor {
         l = 0.43;
         c = 0.09;
         h = 190.0;
-        hex = "#25606b";
+        hex = "25606b";
       };
 
       # Focus (Blue) - h240
@@ -243,19 +243,19 @@ in
         l = 0.68;
         c = 0.18;
         h = 240.0;
-        hex = "#5a7dcf";
+        hex = "5a7dcf";
       };
       Lc60-h240 = mkColor {
         l = 0.53;
         c = 0.16;
         h = 240.0;
-        hex = "#3a5da3";
+        hex = "3a5da3";
       };
       Lc45-h240 = mkColor {
         l = 0.38;
         c = 0.14;
         h = 240.0;
-        hex = "#1f4077";
+        hex = "1f4077";
       };
 
       # Special (Purple) - h290
@@ -263,19 +263,19 @@ in
         l = 0.68;
         c = 0.16;
         h = 290.0;
-        hex = "#a368cf";
+        hex = "a368cf";
       };
       Lc60-h290 = mkColor {
         l = 0.53;
         c = 0.14;
         h = 290.0;
-        hex = "#7d49a3";
+        hex = "7d49a3";
       };
       Lc45-h290 = mkColor {
         l = 0.38;
         c = 0.12;
         h = 290.0;
-        hex = "#592e77";
+        hex = "592e77";
       };
     };
 
@@ -285,19 +285,19 @@ in
         l = 0.71;
         c = 0.20;
         h = 130.0;
-        hex = "#4db368";
+        hex = "4db368";
       };
       Lc60-h130 = mkColor {
         l = 0.56;
         c = 0.18;
         h = 130.0;
-        hex = "#2d8f47";
+        hex = "2d8f47";
       };
       Lc45-h130 = mkColor {
         l = 0.41;
         c = 0.15;
         h = 130.0;
-        hex = "#1b6a2f";
+        hex = "1b6a2f";
       };
 
       # Danger/Error (Red) - h040
@@ -305,19 +305,19 @@ in
         l = 0.64;
         c = 0.23;
         h = 40.0;
-        hex = "#d9574a";
+        hex = "d9574a";
       };
       Lc60-h040 = mkColor {
         l = 0.49;
         c = 0.21;
         h = 40.0;
-        hex = "#a93a2e";
+        hex = "a93a2e";
       };
       Lc45-h040 = mkColor {
         l = 0.34;
         c = 0.18;
         h = 40.0;
-        hex = "#7a2419";
+        hex = "7a2419";
       };
 
       # Warning/Attention (Yellow-Orange) - h090
@@ -325,19 +325,19 @@ in
         l = 0.79;
         c = 0.15;
         h = 90.0;
-        hex = "#c9a93a";
+        hex = "c9a93a";
       };
       Lc60-h090 = mkColor {
         l = 0.64;
         c = 0.13;
         h = 90.0;
-        hex = "#9d8221";
+        hex = "9d8221";
       };
       Lc45-h090 = mkColor {
         l = 0.49;
         c = 0.11;
         h = 90.0;
-        hex = "#715e0e";
+        hex = "715e0e";
       };
 
       # Info (Cyan) - h190
@@ -345,19 +345,19 @@ in
         l = 0.73;
         c = 0.12;
         h = 190.0;
-        hex = "#5aabb9";
+        hex = "5aabb9";
       };
       Lc60-h190 = mkColor {
         l = 0.58;
         c = 0.11;
         h = 190.0;
-        hex = "#3d8592";
+        hex = "3d8592";
       };
       Lc45-h190 = mkColor {
         l = 0.43;
         c = 0.09;
         h = 190.0;
-        hex = "#25606b";
+        hex = "25606b";
       };
 
       # Focus (Blue) - h240
@@ -365,19 +365,19 @@ in
         l = 0.68;
         c = 0.18;
         h = 240.0;
-        hex = "#5a7dcf";
+        hex = "5a7dcf";
       };
       Lc60-h240 = mkColor {
         l = 0.53;
         c = 0.16;
         h = 240.0;
-        hex = "#3a5da3";
+        hex = "3a5da3";
       };
       Lc45-h240 = mkColor {
         l = 0.38;
         c = 0.14;
         h = 240.0;
-        hex = "#1f4077";
+        hex = "1f4077";
       };
 
       # Special (Purple) - h290
@@ -385,19 +385,19 @@ in
         l = 0.68;
         c = 0.16;
         h = 290.0;
-        hex = "#a368cf";
+        hex = "a368cf";
       };
       Lc60-h290 = mkColor {
         l = 0.53;
         c = 0.14;
         h = 290.0;
-        hex = "#7d49a3";
+        hex = "7d49a3";
       };
       Lc45-h290 = mkColor {
         l = 0.38;
         c = 0.12;
         h = 290.0;
-        hex = "#592e77";
+        hex = "592e77";
       };
     };
   };
@@ -410,7 +410,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 40.0;
-        hex = "#d17a5f";
+        hex = "d17a5f";
       };
 
       # GA02 - Green
@@ -418,7 +418,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 177.5;
-        hex = "#5dc7a8";
+        hex = "5dc7a8";
       };
 
       # GA03 - Magenta
@@ -426,7 +426,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 315.0;
-        hex = "#d985c2";
+        hex = "d985c2";
       };
 
       # GA04 - Yellow-Green
@@ -434,7 +434,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 92.5;
-        hex = "#a7b855";
+        hex = "a7b855";
       };
 
       # GA05 - Blue
@@ -442,7 +442,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 230.0;
-        hex = "#7a96e0";
+        hex = "7a96e0";
       };
 
       # GA06 - Orange
@@ -450,7 +450,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 67.5;
-        hex = "#d59857";
+        hex = "d59857";
       };
 
       # GA07 - Cyan
@@ -458,7 +458,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 205.0;
-        hex = "#65b4d9";
+        hex = "65b4d9";
       };
 
       # GA08 - Pink
@@ -466,7 +466,7 @@ in
         l = 0.75;
         c = 0.15;
         h = 342.5;
-        hex = "#e07596";
+        hex = "e07596";
       };
     };
 
@@ -476,7 +476,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 40.0;
-        hex = "#b8664a";
+        hex = "b8664a";
       };
 
       # GA02 - Green
@@ -484,7 +484,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 177.5;
-        hex = "#49a88d";
+        hex = "49a88d";
       };
 
       # GA03 - Magenta
@@ -492,7 +492,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 315.0;
-        hex = "#b86da7";
+        hex = "b86da7";
       };
 
       # GA04 - Yellow-Green
@@ -500,7 +500,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 92.5;
-        hex = "#8c9942";
+        hex = "8c9942";
       };
 
       # GA05 - Blue
@@ -508,7 +508,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 230.0;
-        hex = "#6179be";
+        hex = "6179be";
       };
 
       # GA06 - Orange
@@ -516,7 +516,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 67.5;
-        hex = "#b97d43";
+        hex = "b97d43";
       };
 
       # GA07 - Cyan
@@ -524,7 +524,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 205.0;
-        hex = "#5296b8";
+        hex = "5296b8";
       };
 
       # GA08 - Pink
@@ -532,7 +532,7 @@ in
         l = 0.65;
         c = 0.15;
         h = 342.5;
-        hex = "#be5f7c";
+        hex = "be5f7c";
       };
     };
   };
