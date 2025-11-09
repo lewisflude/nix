cd418fc9e452c31ef23ace99f8e120978c5f0dbf
lewis <lewis@lewisflude.com>
2025-11-09 12:35:23 +0000

    fix: use { ... } for Home Manager modules that receive named args
    - polkit-gnome.nix and wlsunset.nix receive named arguments (system, etc.)
    - Change from { _ } to { ... } to accept any named arguments
    - Configure statix to ignore these files (required for Home Manager compatibility)

--- lib/output-builders.nix
+++ lib/output-builders.nix
@@ -26,7 +26,7 @@ in
           deadnix.enable = true;
           statix = {
             enable = true;
-            entry = "${nixpkgs.legacyPackages.${system}.statix}/bin/statix check --format errfmt --ignore '**/systems.nix'";
+            entry = "${nixpkgs.legacyPackages.${system}.statix}/bin/statix check --format errfmt --ignore '**/systems.nix' --ignore '**/polkit-gnome.nix' --ignore '**/wlsunset.nix'";
           };
           commitizen.enable = true;
 
