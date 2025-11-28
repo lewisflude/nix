657ed90101b101a0ea170ff3f05e2566fd725d45
lewis <lewis@lewisflude.com>
2025-11-28 11:08:12 +0000

    fix(audio): prevent WirePlumber rules from being overwritten Changed lowlatency config from replacing alsa_monitor.rules to appending with table.insert(). This fixes audio routing to AirPlay instead of Apogee Symphony Desktop.

--- modules/nixos/features/desktop/audio/pipewire.nix
+++ modules/nixos/features/desktop/audio/pipewire.nix
@@ -54,17 +54,16 @@ in
             })
           '')
           (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
-            alsa_monitor.rules = {
-              {
-                matches = {{{ "node.name", "matches", "alsa_output.*" }}};
-                apply_properties = {
-                  ["audio.format"] = "S32LE",
-                  ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
-                  ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
-                  -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
-                },
+            -- Add low-latency ALSA configuration to existing rules
+            table.insert(alsa_monitor.rules, {
+              matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+              apply_properties = {
+                ["audio.format"] = "S32LE",
+                ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+                ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+                -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
               },
-            }
+            })
           '')
           # Prevent Apogee Symphony from suspending during screen lock
           # Professional audio interfaces should maintain active state to avoid:
