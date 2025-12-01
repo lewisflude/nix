195f1b3ecdfa625527edd13f68076be366111479
lewis <lewis@lewisflude.com>
2025-12-01 11:42:15 +0000

    feat: add ssh key

--- hosts/jupiter/configuration.nix
+++ hosts/jupiter/configuration.nix
@@ -18,6 +18,7 @@
       hashedPassword = null;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPeK0wgNYUtZScvg64MoZObPaqjaDd7Gdj4GBsDcqAt7 lewis@lewisflude.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEyBDIzK/OoFY7M1i96wP9wE+OeKk56iTvPwStEiFc+k lewis@lewisflude.com" # mercury
         "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBGB2FdscjELsv6fQ4dwLN7ky3Blye+pxJHBfACdYmxhgPodPaRLqbekyrt+XDdXvQYmuiZ0XIa/fL4/452g5MWcAAAAEc3NoOg== lewis@lewisflude.com"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuc2y4LO/GMf02/as8OqUB+zKl+sU44umYXNVC7KzF9 termix@phone"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAiJX39eDkzIc3zWlr/u0nXmzZObmS6wQ7GPgYFt5I80 iphone@lewis"
@@ -60,7 +61,10 @@
 
     # Firewall configuration
     firewall = {
-      allowedTCPPorts = [ constants.ports.mcp.docs ]; # Docs MCP Server HTTP interface
+      allowedTCPPorts = [
+        22 # SSH
+        constants.ports.mcp.docs # Docs MCP Server HTTP interface
+      ];
     };
   };
 
