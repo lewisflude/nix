951f94a804217bd84d22f220fb56197f96b7f1da
lewis <lewis@lewisflude.com>
2025-12-06 14:45:30 +0000

    refactor(mcp): simplify configuration and remove wrapper builders

--- home/nixos/apps/gaming.nix
+++ home/nixos/apps/gaming.nix
@@ -7,6 +7,13 @@ let
     '';
     runtimeInputs = [ pkgs.coreutils ];
   };
+
+  # Lutris launcher that uses systemd to get proper file descriptor limits
+  lutris-systemd = pkgs.writeShellScriptBin "lutris-systemd" ''
+    exec ${pkgs.systemd}/bin/systemd-run --user --scope --unit=lutris \
+      --property="LimitNOFILE=1048576" \
+      ${pkgs.lutris}/bin/lutris "$@"
+  '';
 in
 {
   programs.mangohud = {
@@ -23,5 +30,23 @@ in
     pkgs.wine
     pkgs.winetricks
     steamRunUrl
+    lutris-systemd
   ];
+
+  # Create a desktop entry that launches Lutris with proper ESYNC limits
+  # This overrides the default Lutris desktop entry to use systemd-run
+  # Note: This assumes Lutris is installed at the system level via gaming feature
+  xdg.desktopEntries.lutris = {
+    name = "Lutris";
+    comment = "Install and play games (with ESYNC support)";
+    exec = "${lutris-systemd}/bin/lutris-systemd %U";
+    icon = "lutris";
+    categories = [ "Game" ];
+    mimeType = [
+      "x-scheme-handler/lutris"
+      "application/x-lutris-game"
+    ];
+    terminal = false;
+    type = "Application";
+  };
 }
