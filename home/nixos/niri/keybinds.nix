799a7e1e27e29dcd55c79e16b7c2024ab4b104f4
lewis <lewis@lewisflude.com>
2026-01-13 11:30:48 +0000

    chore: remove redundant niri keybinds file

--- home/nixos/niri/keybinds.nix
+++
@@ -1,4 +0,0 @@
-# Backward compatibility shim - redirects to new modular structure
-# This file is kept for compatibility with existing imports
-# New code should import home/nixos/niri/keybinds/default.nix directly
-import ./keybinds/default.nix
