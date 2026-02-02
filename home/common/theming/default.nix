077c4f9114f07446c1423451316e9110253d5260
lewisflude <lewis@lewisflude.com>
2026-02-02 10:35:00 +0000

    feat: nix-darwin fixes

--- home/common/theming/default.nix
+++ home/common/theming/default.nix
@@ -1,5 +1,15 @@
-{ ... }:
 {
+  lib,
+  system,
+  ...
+}:
+let
+  # Use system from specialArgs to avoid pkgs recursion
+  isLinux = lib.hasSuffix "-linux" system;
+in
+# signal-nix theming is Linux-only (GTK theme)
+# Use optionalAttrs to completely omit the option on Darwin
+lib.optionalAttrs isLinux {
   theming.signal = {
     enable = true;
     autoEnable = true;
