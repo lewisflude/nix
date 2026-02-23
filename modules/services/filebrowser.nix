a791e4e8bf307900d87447c50866c02c3f0793a2
lewisflude <lewis@lewisflude.com>
2026-02-23 17:13:00 +0000

    feat: fix filebrowser perms

--- modules/services/filebrowser.nix
+++ modules/services/filebrowser.nix
@@ -7,7 +7,7 @@ let
   musicDir = "/home/${username}/Music";
 in
 {
-  flake.modules.nixos.filebrowser = { pkgs, ... }: {
+  flake.modules.nixos.filebrowser = {
     services.filebrowser = {
       enable = true;
       group = "media";
@@ -22,20 +22,13 @@ in
 
     systemd.tmpfiles.rules = [
       "d /mnt/storage 0770 root media -"
-      # Traverse home dir so /mnt/storage/Music symlink resolves
-      "a /home/${username} - - - - u:filebrowser:x"
-      # Recursive ACL on storage contents (A+ doesn't follow symlinks,
-      # so ~/Music is handled separately below)
+      "d /mnt/storage/Music 0770 root media -"
       "A+ /mnt/storage - - - - g:media:rX"
-      # Recursive ACL on the real Music path (symlink target)
-      "A+ ${musicDir} - - - - g:media:rX"
     ];
 
-    # Set default ACLs so newly added files inherit media group access.
-    # Uses setfacl instead of tmpfiles A+ with d: prefix to work around
-    # systemd bug #9545 (d: entries fail on files during recursion).
-    systemd.services.filebrowser.serviceConfig.ExecStartPre = "+${pkgs.acl}/bin/setfacl -R -d -m g:media:rX ${musicDir}";
-
-    systemd.services.filebrowser.after = [ "mnt-storage.mount" ];
+    systemd.services.filebrowser = {
+      after = [ "mnt-storage.mount" ];
+      serviceConfig.BindPaths = [ "${musicDir}:/mnt/storage/Music" ];
+    };
   };
 }
