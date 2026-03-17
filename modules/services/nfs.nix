faf8a3843d918c2a104c6926010d589c828098f9
lewisflude <lewis@lewisflude.com>
2026-03-17 14:56:34 +0000

    feat: update flake

--- modules/services/nfs.nix
+++ modules/services/nfs.nix
@@ -1,11 +1,11 @@
 # NFS File Sharing
 # Server: Export ~/Music from Jupiter for Ableton on Mercury
-# Client: macOS autofs (on-demand mount at /Volumes/music)
+# Client: macOS autofs (on-demand mount at /mnt/music)
 { config, ... }:
 let
   jupiterIp = config.constants.hosts.jupiter.ipv4;
   mercuryIp = config.constants.hosts.mercury.ipv4;
-  nfsOpts = "fstype=nfs,resvport,soft,intr,rsize=32768,wsize=32768,timeo=10,retrans=3";
+  nfsOpts = "fstype=nfs,resvport,soft,intr,bg,rsize=32768,wsize=32768,timeo=10,retrans=3";
 in
 {
   # NixOS server: NFS export
@@ -35,10 +35,20 @@ in
     ];
   };
 
-  # macOS client: autofs on-demand NFS mount
+  # macOS client: autofs on-demand NFS mount at /mnt/music
   flake.modules.darwin.nfs = _: {
+    # /mnt is on the writable data volume (macOS root is read-only since Catalina)
+    environment.etc."synthetic.conf" = {
+      text = ''
+        mnt
+      '';
+      knownSha256Hashes = [
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" # empty file
+      ];
+    };
+
     environment.etc."auto_nfs".text = ''
-      /Volumes/music -${nfsOpts} ${jupiterIp}:/home/${config.username}/Music
+      /System/Volumes/Data/mnt/music -${nfsOpts} ${jupiterIp}:/home/${config.username}/Music
     '';
 
     environment.etc."auto_master".text = ''
@@ -52,5 +62,14 @@ in
       /-			-static
       /-			auto_nfs	-nobrowse,nosuid
     '';
+
+    # Reload autofs at boot to pick up nix-darwin managed config
+    launchd.daemons.reload-autofs = {
+      serviceConfig = {
+        Label = "org.nix.reload-autofs";
+        ProgramArguments = [ "/bin/sh" "-c" "automount -cv" ];
+        RunAtLoad = true;
+      };
+    };
   };
 }
